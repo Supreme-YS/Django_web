f06cb7c3ddba2e5befa41609d638adacbaf424eb
Supreme-YS <dudtjr1225@gmail.com>
2021-02-27 18:08:58 +0900

    TIL

--- django-web/practice/mysite/polls/admin.py
+++ django-web/practice/mysite/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Admin 사이트에 반영
+from polls.models import Question, Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
